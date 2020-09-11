8ec4ab3b6c53dbb48971c100ab9d13201c90a9ea
DevM-uk <maxbj910@hotmail.co.uk>
2020-09-11 19:49:08 +0100

    Add student_visualisations python file.

---
+++ ai4good/webapp/student_visualisations.py
@@ -0,0 +1,3 @@
+# Please add your own visualisations to this file.
+# Lay them out similarly to how they are in plotter.py so that they are modular and can be used in various places. (Each visualisation has its own subroutine.)
+# Also if you could add a comment next to the definition of each subroutine with the names of the people who worked on it, that would be great. -Max
