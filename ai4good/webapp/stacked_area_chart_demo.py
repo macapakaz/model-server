d2be6699dd5324803b43d818739ca0cb63108fad
DevM-uk <maxbj910@hotmail.co.uk>
2020-09-18 00:49:16 +0100

    Updated stacked area chart. Added demo code and comments.

---
+++ ai4good/webapp/stacked_area_chart_demo.py
@@ -0,0 +1,81 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+from student_visualisations import AgeBreakdown
+
+external_stylesheets = ["https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+categories = ["Susceptible", "Exposed", "Infected (symptomatic)", "Asymptomatically Infected", "Recovered", "Hospitalised", "Critical", "Deaths", "Offsite", "Quarantined", "No ICU Care"]
+ageCategories = ["0-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70+"]
+'''
+Categories:
+ Susceptible
+ Exposed
+ Infected (symptomatic)
+ Asymptomatically Infected
+ Recovered
+ Hospitalised
+ Critical
+ Deaths
+ Offsite
+ Quarantined
+ No ICU Care
+'''
+
+app.layout = html.Div(children=[
+    html.H1(children="Stacked area chart demonstration"),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id="age_breakdown_area_chart"#,
+        #figure=AgeBreakdown("Asymptomatically Infected", showAsPercent=True)
+    ),
+    html.Div([
+        dcc.Dropdown(
+                id="input_category",
+                options=[{"label": i, "value": i} for i in categories],
+                value="Asymptomatically Infected",
+            ),
+        dcc.RadioItems(
+                id="input_percentage",
+                options=[{"label": "Percentage", "value": "Percentage"}, {"label": "Decimal", "value": "Decimal"} ],
+                value="Percentage",
+                labelStyle={"display": "inline-block"}
+            ),
+        "Days: ",
+        dcc.Input(
+                id='input_days',
+                type='number',
+                value=250,
+                min=0,
+                max=800
+            )
+        ],
+        style={'width': '50%', 'display': 'inline-block'})
+])
+
+@app.callback(
+    Output("age_breakdown_area_chart", "figure"),
+    [Input("input_category", "value"),
+     Input("input_percentage", "value"),
+     Input("input_days", "value")]
+    )
+def Update_graph(input_category, input_percentage, input_days):
+    if (input_percentage == "Percentage"):
+        input_percentage = True
+    else:
+        input_percentage = False
+    return AgeBreakdown(category=input_category, showAsPercent=input_percentage, days=input_days)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
--- ai4good/webapp/student_visualisations.py
+++ ai4good/webapp/student_visualisations.py
@@ -3,81 +3,75 @@
 # Also if you could add a comment next to the definition of each subroutine with the names of the people who worked on it, that would be great. -Max
 #------------------------------------------------------------------
 
+import pandas as pd     
+import plotly.express as px
+import plotly.graph_objects as go
+
 #Age breakdown stacked area chart -Max -----------------------------------------------------------------
 def AgeBreakdown(category = None, days = 250, showAsPercent = False, df = None):
+    categories = ["Susceptible", "Exposed", "Infected (symptomatic)", "Asymptomatically Infected", "Recovered", "Hospitalised", "Critical", "Deaths", "Offsite", "Quarantined", "No ICU Care"]
+    ageCategories = ["0-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70+"]
     if (df == None):
-        data = pd.read_csv("expected_report.csv")
+        data = pd.read_csv("..\\runner\\tests\\expected_report.csv") #Read the data from a csv file (model output can be exported as csv).
     else:
-        data = df
+        data = df #Alternatively a DataFrame can be passed in containing the data.
     columns = []
     for item in data.columns:
-        #print(item)
-        columns.append(item)
+        columns.append(item) #Generate a list of all category names
 
     try:
         categories.index(category)
-        selectedCategory = category
+        selectedCategory = category #Detect and check the selected category.
     except ValueError:
-        selectedCategory = "Asymptomatically Infected"
+        selectedCategory = "Asymptomatically Infected" #If category invalid, choose Asymptomatically Infected.
 
-    #selectedCategory = "Asymptomatically Infected"
     dataColumnIndexes = []
     dataColumnIndexesRemoval = []
     selectedCategory = selectedCategory + ":"
     dataColumnNames = []
-    for i in range(0, len(columns)-1):
+    for i in range(0, len(columns)-1): #Get the names of all the columns which match the selected category in the DataFrame.
         testCategory = columns[i]
         if (testCategory[0:len(selectedCategory)] == selectedCategory):
-            dataColumnIndexes.append(i)
             dataColumnNames.append(testCategory)
-        else:
-            dataColumnIndexesRemoval.append(i)
 
-    print(dataColumnNames)
-    #print(data[dataColumnNames].copy())
-    #input()
-    #print(data[dataColumnNames].head(250).copy())
     dataToPlot = data[dataColumnNames].head(days).copy()
     if (showAsPercent):
-        dataToPlot = dataToPlot.multiply(100).copy()
+        dataToPlot = dataToPlot.multiply(100).copy() #Multiply the decimal values by 100 if they are to be displayed as percentages.
 
     fig = go.Figure()
     x = dataToPlot.columns
-    colours = ["rgb(0, 0, 252)", "rgb(33, 33, 252)", "rgb(66, 66, 252)", "rgb(99, 99, 252)", "rgb(132, 132, 252)", "rgb(165, 165, 252)", "rgb(198, 198, 252)", "rgb(211, 211, 252)"]
+    colours = ["rgb(0, 0, 252)", "rgb(33, 33, 252)", "rgb(66, 66, 252)", "rgb(99, 99, 252)", "rgb(132, 132, 252)", "rgb(165, 165, 252)", "rgb(198, 198, 252)", "rgb(211, 211, 252)"] #Colours for each age group.
     for column in dataToPlot.columns:
-
-        label = str(column)
+        label = str(column) #Set the label for the series to the column name if the column name doesn't contain an age range.
         for ageRange in ageCategories:
             if (ageRange in column):
-                label = ageRange
+                label = ageRange #Set the label for the series to the age group if the column name does contain an age range.
                 break
 
         if (len(colours) == 0):
-            colours.append("rgb(0, 0, 0)")
+            colours.append("rgb(0, 0, 0)") #If the list of colours has run out, use black as a placeholder.
 
         fig.add_trace(go.Scatter(
             name = label,
-            x = list(range(1, days + 1)),
-            y = dataToPlot[column],
+            x = list(range(1, days + 1)), #Set the X data to be the number of days set in the parameter
+            y = dataToPlot[column], #Set the Y data to be the data from the DataFrame
             mode = "lines",
-            line = dict(width=0.5, color=colours.pop()),
-            #hoverinfo = "x+y+name",
-            #hoverlabel=dict(bgcolor="rgb(255, 255, 255)"),
-            stackgroup = "one"
+            line = dict(width=0.5, color=colours.pop()), #Set the line colour and width.
+            stackgroup = "one" #Make sure they are all in the same stackgroup so each line stacks on each other.
             ))
 
     fig.update_layout(
-            showlegend=True,
-            title=go.layout.Title(text=selectedCategory),
+            showlegend=True, #Show the list of legends
+            title=go.layout.Title(text=selectedCategory), #Set the chart title and
             legend_title_text="Age Groups:",
-            plot_bgcolor = "rgb(100, 100, 100)",
+            plot_bgcolor = "rgb(100, 100, 100)", #Background colour for the chart - may need to be changed.
             xaxis=dict(
-                title="Days"
+                title="Days" #Set the title for the x axis.
                 ),
-            hovermode = "x"
+            hovermode = "x" #x unified doesn't work for some reason.
         )
     if (showAsPercent):
-        fig.update_layout(
+        fig.update_layout( #Adjust the y axis label and ticksuffix accordingly depending on whether percentages or decimals are chosen in the parameter.
             yaxis=dict(
                 type='linear',
                 ticksuffix='%',
@@ -91,5 +85,6 @@ def AgeBreakdown(category = None, days = 250, showAsPercent = False, df = None):
                 title="Proportion of population."
                 ),
             )
-    return fig
+    return fig #Return the Plotly figure.
 
+#-------------------------------------------------------
