4a928ece6ec2f3ae0106bdc8aa4b3e950f1b7e76
Joseph Rance <56409230+Joseph-Rance@users.noreply.github.com>
2020-09-18 14:06:42 +0100

    Add sunburst chart demo

---
+++ ai4good/webapp/sunburst_chart_demo.py
@@ -0,0 +1,20 @@
+import dash             
+import dash_core_components as dcc
+import dash_html_components as html
+from visualisations import get_sunburst_values, generate_sunburst
+import pandas as pd
+
+df = pd.read_csv("camp_params.csv")
+df = df[df['Camp']=='Moria']
+
+labels, parents, values = get_sunburst_values(df)
+graph = generate_sunburst(labels, parents, values)
+
+SunburstChart = dash.Dash()
+
+SunburstChart.layout = html.Div([
+    dcc.Graph(figure=graph),
+])
+
+if __name__ == '__main__':
+    SunburstChart.run_server(debug=True)  # Dash is running on http://127.0.0.1:8050/
